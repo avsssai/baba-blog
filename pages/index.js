06f01564d8c40f2df03dfce1b97bf5bb60edad79
avsssai <34810623+avsssai@users.noreply.github.com>
2020-11-23 00:34:08 +0530

    Landing page changed

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,52 @@
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import Layout from "../components/layout";
 import styles from "../styles/index.module.css";
 import Wave from "../public/wave.svg";
 
 export default function Home() {
 	return (
-		<Layout>
-			<div className={styles.content}>
+		<div className={styles.container}>
+			<div className={styles.mainCard} style={{ borderRadius: "10px" }}>
+				<div className={styles.cardContent}>
+					<div className={styles.mainHeader}>
+						<div className={styles.firstName}>Sujay</div>
+						<div className={styles.lastName}>Krishna</div>
+					</div>
+					<div className={styles.intro}>
+						Food <span className={styles.spin}>Writer</span>
+					</div>
+					<div className={styles.links}>
+						<Link href='/blog/home'>
+							<a>
+								Me flexing my finger muscles
+								<div className={styles.underline}></div>
+							</a>
+						</Link>
+						<Link href='#'>
+							<a>
+								Get in touch
+								<div className={styles.underline}></div>
+							</a>
+						</Link>
+						<Link href='#'>
+							<a>
+								Cute pictures of my dog
+								<div className={styles.underline}></div>
+							</a>
+						</Link>
+					</div>
+				</div>
+				<div className={styles.cardImage}>
+					<img src='/images/profile.jpg' />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+{
+	/* <div className={styles.content}>
 				<div className={styles.nameImage}>
 					<div className={styles.name}>
 						<div className={styles.firstName}>Sujay</div>
@@ -46,7 +85,5 @@ export default function Home() {
 			</div>
 			<div className='wave'>
 				<Wave />
-			</div>
-		</Layout>
-	);
+			</div> */
 }
