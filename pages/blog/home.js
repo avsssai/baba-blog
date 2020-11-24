c48a4265af6e3fd0d85328485d595705a2dade70
avsssai <34810623+avsssai@users.noreply.github.com>
2020-11-25 00:11:03 +0530

    started on blog landing, to complete

--- pages/blog/home.js
+++ pages/blog/home.js
@@ -7,38 +7,57 @@ import Layout from "../../components/layout";
 import styles from "../../styles/blogLandingStyles.module.css";
 
 const breakpointColumnsObj = {
-  default: 3,
-  1100: 2,
-  700: 1,
-  500: 1,
+	default: 3,
+	1100: 2,
+	700: 1,
+	500: 1,
 };
 
 export default function BlogLanding({ createdData }) {
-  // console.log(createdData);
-  return (
-    <Layout>
-      <Head>
-        <title>Blog!</title>
-      </Head>
-      <div className={styles.blogLanding}>
-        <div className={styles.details}></div>
-      </div>
-      <div className={styles.blogList}></div>
-      <Link href="/">
-        <a>Back to Home</a>
-      </Link>
-    </Layout>
-  );
+	// console.log(createdData);
+	return (
+		<Layout>
+			<Head>
+				<title>Blog!</title>
+			</Head>
+			<div className={styles.blogLanding}>
+				<div className={styles.contentWrapper}>
+					<div className={styles.details}></div>
+					<div className={styles.blogList}>
+						{createdData.map((item) => {
+							return (
+								<div className={styles.blog}>
+									<div className={styles.blogImage}>
+										<img
+											src={item.image}
+											alt='blog image'
+										/>
+									</div>
+
+									<div className={styles.title}>
+										{item.title}
+									</div>
+									<div className={styles.subText}>
+										Read More
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 }
 
 export async function getStaticProps() {
-  return {
-    props: { createdData },
-  };
+	return {
+		props: { createdData },
+	};
 }
 
 {
-  /* <Masonry
+	/* <Masonry
           breakpointCols={breakpointColumnsObj}
           className={styles.myMasonryGrid}
           columnClassName={styles.myMasonryGridColumn}
